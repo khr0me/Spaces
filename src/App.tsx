57a44ef7d79b03854ad8015e9591c009b6096b30
Stefano Casonato <121874632+khr0me@users.noreply.github.com>
2025-04-08 11:13:06 +0200

    spaces title

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,7 @@ function App() {
 
   return (
     <div className="p-4 max-w-md mx-auto">
-      <h1 className="text-xl font-bold mb-2">
-        Mini App + Vite + TS + React + Wagmi
-      </h1>
+      <h1 className="text-xl font-bold mb-2">Spaces</h1>
       <ConnectMenu />
       <RoomList />
     </div>
